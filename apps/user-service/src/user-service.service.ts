9f216d51bda460eb12117d107228d305da64ed97
thejit <jeonghui1791@gmail.com>
2022-03-20 23:54:11 +0900

    add db connection

--- apps/user-service/src/user-service.service.ts
+++ apps/user-service/src/user-service.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDto } from './create-user-dto';
+import { Users } from './user.model';
 
 @Injectable()
 export class UserServiceService {
+  constructor(
+    @InjectModel(Users)
+    private readonly userModel: typeof Users,
+  ) {}
+
+  async findAll(): Promise<Users[]> {
+    return this.userModel.findAll();
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<Users> {
+    return this.userModel.create(<Users>createUserDto);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
