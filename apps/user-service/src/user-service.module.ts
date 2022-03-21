18a956c04677aafa1332b9ada6977c63e5e6df4b
thejit <jeonghui1791@gmail.com>
2022-03-21 11:03:28 +0900

    add async conn
    - multi db
    - Nest can't resolve dependencies of the UserServiceService (?)

--- apps/user-service/src/user-service.module.ts
+++ apps/user-service/src/user-service.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './mongoose-config-service';
+import { SequelizeConfigService } from './sequelize-config-service';
+import { UserConfigModule } from './user-config.module';
 import { UserServiceController } from './user-service.controller';
 import { UserServiceService } from './user-service.service';
 import { Users } from './user.model';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'test',
-      password: 'tpass',
-      database: 'testdb',
-      autoLoadModels: true,
-      synchronize: true,
-      models: [Users],
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [UserConfigModule],
+      // name: 'development',
+      useExisting: SequelizeConfigService,
     }),
     SequelizeModule.forFeature([Users]),
   ],
